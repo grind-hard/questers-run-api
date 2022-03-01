8cc2fcc41a23638841b6eb27b44ec899760b7e44
Andrew Grinder <grinder.andrew2@gmail.com>
2022-02-28 23:11:59 -0600

    bindings update

--- src/functions/bindings/users.bindings.ts
+++ src/functions/bindings/users.bindings.ts
@@ -3,10 +3,15 @@ import { AzureFunctionSchema } from 'ts-func'
 export const users: AzureFunctionSchema = {
   bindings: [
     {
-      name: 'usersRequest',
+      name: 'request',
       type: 'httpTrigger',
       direction: 'in',
       methods: ['get', 'post', 'delete', 'put', 'patch']
+    },
+    {
+      name: 'response',
+      type: 'http',
+      direction: 'out'
     }
   ],
   scriptFile: '../dist/src/functions/src/users/users.http.js'
