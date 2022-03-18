9de2dfd1a12c41d93086b96d02def60d0be87356
Andrew Grinder <grinder.andrew2@gmail.com>
2022-03-17 18:58:53 -0600

    updated armor comments

--- src/interfaces/entities/game/armor.ts
+++ src/interfaces/entities/game/armor.ts
@@ -4,18 +4,28 @@ import { EntityBase } from '../entities.base'
  * Armor is meant for Characters, NPCs, Mobs, and Bosses.
  */
 export interface Armor extends EntityBase {
+  /** Armor value. Negates incoming physical damage. */
   value: number
+  /** The total durability of the armor. */
+  totalDurability: number
+  /** The current durability of the armor. */
   durability: number
+  /** The character level requirement to equip the armor. */
   levelRequirement: number
+  /** The price of the armor when selling to a vendor. */
   price: number
+  /** The armor's type. */
   type: ArmorTypeClassification
+  /** The slot the armor is equipped to. */
   slot: SlotTypeClassification
 }
 
+/** Customizable armor types. */
 export interface ArmorTypeClassification extends EntityBase {
 
 }
 
+/** Customizable armor slot types. eg. Head, Neck, etc... */
 export interface SlotTypeClassification extends EntityBase {
 
 }
