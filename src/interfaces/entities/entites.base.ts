1c2ed5c72429b9e2fdc33700fc92769ad4abe57a
Andrew Grinder <grinder.andrew2@gmail.com>
2022-03-17 18:43:47 -0600

    updated entities base

--- src/interfaces/entities/entites.base.ts
+++ src/interfaces/entities/entites.base.ts
@@ -1,5 +1,11 @@
+/**
+ * EntityBase contains the common properties of all other entities.
+ */
 export interface EntityBase {
+  /** The unique identifier of the entity. */
   id: string
+  /** The title of the entity. */
   title: string
+  /** The description of the entity. */
   description: string
 }
