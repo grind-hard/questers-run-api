72d4a33c1774f035d4c4132efab60a48316682bd
Andrew Grinder <grinder.andrew2@gmail.com>
2022-03-17 18:40:37 -0600

    updated universe with comments

--- src/interfaces/entities/game/universe.ts
+++ src/interfaces/entities/game/universe.ts
@@ -1,5 +1,13 @@
 import { EntityBase } from '../entites.base'
 
+/**
+ * There can be many Universes on a single server.
+ * Universes may connect with each other for various activities.
+ * A Universe can have many Worlds.
+ */
 export interface Universe extends EntityBase {
-
+	/** The child world ids associated with the Universe. */
+	worldIds: string[]
+	/** Related Universes to this one. */
+	siblingUniverseIds: string[]
 }
