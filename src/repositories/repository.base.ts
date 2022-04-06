77495a1bf285a8e898d861337ae3e614c58f3cb1
Andrew Grinder <grinder.andrew2@gmail.com>
2022-04-05 21:43:42 -0500

    removed repos

--- src/repositories/repository.base.ts
+++
@@ -1,7 +0,0 @@
-export interface BaseApiRepository<T> {
-	get(): Promise<T[]>
-	getById(id: string): Promise<T>
-	add(o: T): Promise<T>
-	update(o: T): Promise<T>
-	delete(id: string): Promise<boolean>
-}
